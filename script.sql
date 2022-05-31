14bfcce608821693694f45e883eb80205d91d3df
alearredondo <91506416+alearredondo@users.noreply.github.com>
2022-05-30 22:39:40 -0500

    final commit

--- script.sql
+++ script.sql
@@ -1,3 +1,9 @@
+--Tarea 4
+--Alejandra Arredondo Hernandez 189744
+--NOTA: como se recomendo en clase, Alejandra, Elisa y Yuliana compartimos nuestros conocimientos 
+--después de haber elaborado de manera individual nuestra tarea, cada una sabe como hacer cada pregunta, 
+--sin embargo, intercambiamos opiniones y formas de hacerlo para enriquecer nuestro conocimiento en la materia.
+
 --Pregunta 1, ¿Cuál es el promedio, en formato human-readable, de tiempo entre cada pago por cliente de la BD Sakila?
 select concat(c.first_name, ' ', c.last_name) as nombre_completo_cliente, 
 (max(p.payment_date)-min(p.payment_date))/(count(*)) as promedio
@@ -49,6 +55,19 @@ $func$ LANGUAGE plpgsql;
 --Query
 select * from histogram('(select extract(epoch from (max(p.payment_date) - min(p.payment_date))/(count(*))) as promedio
 from payment p group by customer_id order by promedio) as histoprom' ,'promedio');
+
+
+--Pregunta 3, ¿Qué tanto difiere ese promedio del tiempo entre rentas por cliente?
+select concat( c.first_name, ' ', c.last_name ) as nombre_completo_cliente, 
+(max(r.rental_date) - min(r.rental_date))/(count(*)) as rentas_promedio,  
+(max(p.payment_date) - min(p.payment_date))/(count(*)) as pagos_promedio, 
+(max(r.rental_date) - min(r.rental_date))/(count(*)) - 
+(max(p.payment_date) - min(p.payment_date))/(count(*)) as diferencia_promedio
+from payment p join customer c using(customer_id) join rental r using(customer_id)
+group by customer_id order by 2 desc;
+
+
+
 
 
 
